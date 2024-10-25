7b47132e5e118984f0b896b11b61fc12be3a2307
Joy Biswas <joybiswas389@gmail.com>
2024-10-25 21:12:28 +0600

    logo updated on auth pages

--- src/components/Authentication/SignIn.jsx
+++ src/components/Authentication/SignIn.jsx
@@ -42,9 +42,10 @@ export default function SignIn() {
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+
                 <img
-                    alt="Your Company"
-                    src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="TeleBuzzed Logo"
+                    src="/telebuzzed-logo-black.png"
                     className="mx-auto h-10 w-auto"
                 />
                 <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-base-content">
